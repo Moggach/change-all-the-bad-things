371ccc6ce3dbf9848f9b8ccca12fd72415f096fa
“Moggach” <acunnane13@gmail.com>
2021-08-14 15:14:13 +0100

    adding function to handle submission, finishing JS including validation, resetting form and submission

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-function valCheckBoxes() {
+function validateCheckboxes() {
   const email = document.getElementById('contactChoice1');
   const phone = document.getElementById('contactChoice2');
   if (email.checked === false && phone.checked === false) {
-    alert('please select a contact method');
+    alert('Please select a contact method');
     return false;
   }
 }
+
+function showInput() {
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const phone = document.getElementById('phone').value;
+  const message = `name ${name}, email ${email}`;
+  const data = document.getElementById('data');
+  data.innerHTML = message;
+}
+
+function formReset() {
+  document.getElementById('form').reset();
+}
+
+function onSubmit(e) {
+  e.preventDefault();
+  if (validateCheckboxes() == false) {
+    return false;
+  }
+  showInput();
+  formReset();
+  alert('Thank you for your submission. We will be in touch shortly');
+}
+
+document.getElementById('submit').addEventListener('click', onSubmit);
